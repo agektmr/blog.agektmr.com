bbbf8dd3b7c70ecda615041c0e2b9541349be6db
Eiji Kitamura <agektmr+github@gmail.com>
2015-04-01 23:46:19 +0900

    upgrade polymer to 0.5.5

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,20 @@
-document.querySelector('#drawer_toggle').addEventListener('click', function() {
-  var cdp = document.querySelector('core-drawer-panel');
-  if (cdp.clientWidth > parseInt(cdp.responsiveWidth, 10)) {
-    cdp.narrow = !cdp.narrow;
-  } else {
-    cdp.narrow = true;
-    cdp.togglePanel();
-  }
-});
+// document.querySelector('body').addEventListener('click', function(e) {
+//   console.log('click event');
+//   if (e.target.tagName == 'A') {
+//     var domain = RegExp('^(http|https)://'+location.host);
+//     if (e.target.href && domain.test(e.target.href)) {
+//       console.log(e.target.href);
+//       fetch(e.target.href, function(response) {
+//         if (response) {
+//           document.querySelector('core-drawer-panel').innerHTML = response.querySelector('core-drawer-panel').innerHTML;
+//           history.pushState(null, '', e.target.href);
+//         }
+//       });
+//       e.preventDefault();
+//       e.stopPropagation();
+//     }
+//   }
+// });
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js').then(function(sw) {
